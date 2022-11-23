187a0681636808fe84627f32ef751ff3e4da95c8
Some User <guest@example.com>
2022-11-23 23:18:48 +0300

    Apply black formatting

--- procstat/stat.py
+++ procstat/stat.py
@@ -10,30 +10,31 @@ import sys
 from datetime import datetime
 from copy import copy
 
-logger = logging.getLogger('procstat')
+logger = logging.getLogger("procstat")
 
-DEFAULT_MEASUREMENT = 'event'
+DEFAULT_MEASUREMENT = "event"
 
 
 class Stat(object):
     default_key_aliases = {}
     ignore_prefixes = ()
+
     def __init__(
-            self,
-            # logging
-            speed_keys=None,
-            logging_enabled=True,
-            logging_interval=3,
-            logging_format='text',
-            logging_level=logging.DEBUG,
-            key_aliases=None,
-            # export
-            #shard_interval = 10,
-            export=None,
-            export_interval=5,
-            # fatalq
-            fatalq=None,
-        ):
+        self,
+        # logging
+        speed_keys=None,
+        logging_enabled=True,
+        logging_interval=3,
+        logging_format="text",
+        logging_level=logging.DEBUG,
+        key_aliases=None,
+        # export
+        # shard_interval = 10,
+        export=None,
+        export_interval=5,
+        # fatalq
+        fatalq=None,
+    ):
         # Arg: speed_keys
         if speed_keys is None:
             speed_keys = []
@@ -57,7 +58,7 @@ class Stat(object):
         self.fatalq = fatalq
 
         # Arg: shard_interval
-        #self.shard_interval = shard_interval
+        # self.shard_interval = shard_interval
 
         # Logging
         self.total_counters = defaultdict(int)
@@ -74,7 +75,7 @@ class Stat(object):
         self.export_config = export
         self.export_driver = None
         self.th_export_state = {
-            'prev_counters': None,
+            "prev_counters": None,
         }
         self.th_export_lock = Lock()
 
@@ -85,7 +86,7 @@ class Stat(object):
         self.export_interval = export_interval
 
         # Export
-        #self.shard_counters = {}
+        # self.shard_counters = {}
         if self.export_driver:
             self.start_export_thread()
 
@@ -101,16 +102,16 @@ class Stat(object):
 
     def setup_export_driver(self, cfg):
         self.export_config = cfg
-        mod_path, cls_name = cfg['driver'].split(':', 1)
+        mod_path, cls_name = cfg["driver"].split(":", 1)
         driver_mod = import_module(mod_path)
         driver_cls = getattr(driver_mod, cls_name)
         self.export_driver = driver_cls(
-            tags=cfg.get('tags', {}),
-            connect_options=cfg.get('connect_options', {}),
-            measurement=cfg.get('measurement', None)
+            tags=cfg.get("tags", {}),
+            connect_options=cfg.get("connect_options", {}),
+            measurement=cfg.get("measurement", None),
         )
         if self.th_export:
-            raise Exception('Stat export thread already created')
+            raise Exception("Stat export thread already created")
         else:
             self.start_export_thread()
 
@@ -131,17 +132,17 @@ class Stat(object):
         return eps
 
     def build_eps_string(self, now):
-        interval=30
+        interval = 30
         eps = self.build_eps_data(now, interval)
         ret = []
         for key, val in eps.items():
             label = self.key_aliases.get(key, key)
-            val_str = '%.1f' % (val / interval)
-            if val_str == '0.0' and val > 0:
-                val_str = '0.0+'
-            ret.append('%s: %s' % (label, val_str))
+            val_str = "%.1f" % (val / interval)
+            if val_str == "0.0" and val > 0:
+                val_str = "0.0+"
+            ret.append("%s: %s" % (label, val_str))
         ret = sorted(ret, key=lambda x: x[0])
-        return ', '.join(ret)
+        return ", ".join(ret)
 
     def build_counter_data(self, ignore=True):
         ret = {}
@@ -157,25 +158,27 @@ class Stat(object):
         for key in sorted(list(data.keys())):
             label = self.key_aliases.get(key, key)
             val = data[key]
-            ret.append('%s: %d' % (label, val))
-        return ', '.join(ret)
+            ret.append("%s: %d" % (label, val))
+        return ", ".join(ret)
 
     def render_moment_json(self, now):
         interval = 30
-        return json.dumps({
-            'eps': self.build_eps_data(now, interval),
-            'counter': self.build_counter_data(),
-        })
+        return json.dumps(
+            {
+                "eps": self.build_eps_data(now, interval),
+                "counter": self.build_counter_data(),
+            }
+        )
 
     def render_moment(self, now=None):
         if now is None:
             now = time.time()
-        if self.logging_format == 'json':
+        if self.logging_format == "json":
             return self.render_moment_json(now)
         else:
             eps_str = self.build_eps_string(now)
             counter_str = self.build_counter_string()
-            return 'EPS: %s | TOTAL: %s' % (eps_str, counter_str)
+            return "EPS: %s | TOTAL: %s" % (eps_str, counter_str)
 
     def thread_logging(self):
         try:
@@ -183,9 +186,7 @@ class Stat(object):
                 now = time.time()
                 logger.log(self.logging_level, self.render_moment(now))
                 # Sleep `self.logging_interval` seconds minus time spent on logging
-                sleep_time = (
-                    self.logging_interval + (time.time() - now)
-                )
+                sleep_time = self.logging_interval + (time.time() - now)
                 time.sleep(sleep_time)
         except (KeyboardInterrupt, Exception) as ex:
             if self.fatalq:
@@ -198,16 +199,16 @@ class Stat(object):
         released = False
         try:
             counters = deepcopy(self.total_counters)
-            if self.th_export_state['prev_counters']:
+            if self.th_export_state["prev_counters"]:
                 # pytype: disable=attribute-error
                 delta_counters = dict(
-                    (x, counters[x] - self.th_export_state['prev_counters'].get(x, 0))
+                    (x, counters[x] - self.th_export_state["prev_counters"].get(x, 0))
                     for x in counters.keys()
                 )
                 # pytype: enable=attribute-error
             else:
                 delta_counters = counters
-            self.th_export_state['prev_counters'] = counters
+            self.th_export_state["prev_counters"] = counters
             self.th_export_lock.release()
             released = True
             self.export_driver.write_events(delta_counters)
@@ -220,9 +221,7 @@ class Stat(object):
             while True:
                 ts = time.time()
                 self.th_export_dump_stat()
-                sleep_time = (
-                    self.export_interval - (time.time() - ts)
-                )
+                sleep_time = self.export_interval - (time.time() - ts)
                 if sleep_time > 0:
                     time.sleep(sleep_time)
         except (KeyboardInterrupt, Exception) as ex:
@@ -235,12 +234,12 @@ class Stat(object):
 
     def inc(self, key, count=1):
         now_int = int(time.time())
-        #shard_ts = now_int - now_int % self.shard_interval
-        #shard_slot = self.shard_counters.setdefault(shard_ts, defaultdict(int))
+        # shard_ts = now_int - now_int % self.shard_interval
+        # shard_slot = self.shard_counters.setdefault(shard_ts, defaultdict(int))
         moment_slot = self.moment_counters.setdefault(now_int, defaultdict(int))
 
         moment_slot[key] += count
-        #shard_slot[key] += count
+        # shard_slot[key] += count
         self.total_counters[key] += count
 
 
@@ -281,22 +280,20 @@ class InfluxdbExportDriver(object):
         try:
 
             if not self.database_created:
-                self.client.create_database(self.connect_options['database'])
+                self.client.create_database(self.connect_options["database"])
                 self.database_created = True
             if snapshot:
                 data = {
                     "measurement": (measurement or self.default_measurement),
                     "tags": merged_tags,
                     "time": datetime.utcnow().isoformat(),
-                    "fields": dict((
-                        (x, y) for x, y in snapshot.items()
-                    )),
+                    "fields": dict(((x, y) for x, y in snapshot.items())),
                 }
                 while True:
                     try:
                         self.client.write_points([data])
                     except retry_exceptions:
-                        logger.exception('Failed to send metrics')
+                        logger.exception("Failed to send metrics")
                         time.sleep(1)
                         # reconnecting
                         while True:
@@ -304,7 +301,7 @@ class InfluxdbExportDriver(object):
                                 self.connect()
                             except retry_exceptions:
                                 logger.exception(
-                                    'Failed to reconnect to metrics database'
+                                    "Failed to reconnect to metrics database"
                                 )
                                 time.sleep(1)
                             else:
@@ -312,20 +309,96 @@ class InfluxdbExportDriver(object):
                     else:
                         break
         except Exception as ex:
-            logging.exception('ERROR IN STAT WRITE EVENTS')
+            logging.exception("ERROR IN STAT WRITE EVENTS")
             raise
 
 
 class CrawlerInfluxdbExportDriver(InfluxdbExportDriver):
     def __init__(self, connect_options, tags, *args, **kwargs):
-        for key in ['hostname', 'project', 'crawler_id']:
+        for key in ["hostname", "project", "crawler_id"]:
             if key not in tags:
                 raise Exception(
-                    'Tag %s is required to use CalwerInfluxdbExportDriver'
-                    % key
+                    "Tag %s is required to use CalwerInfluxdbExportDriver" % key
                 )
-        if not kwargs.get('measurement'):
-            kwargs['measurement'] = 'crawler_stats'
+        if not kwargs.get("measurement"):
+            kwargs["measurement"] = "crawler_stats"
         super(CrawlerInfluxdbExportDriver, self).__init__(
             connect_options, tags, *args, **kwargs
         )
+
+
+class Influxdb2ExportDriver(object):
+    def __init__(self, connect_options, tags=None, measurement=None):
+        self.connect_options = deepcopy(connect_options)
+        self.client = None
+        if measurement:
+            self.default_measurement = measurement
+        else:
+            self.default_measurement = DEFAULT_MEASUREMENT
+        self.tags = deepcopy(tags or {})
+        self.database_created = False
+        self.connect()
+
+    def connect(self):
+        from influxdb_client import InfluxDBClient
+        from influxdb_client.client.write_api import SYNCHRONOUS
+
+        self.client = InfluxDBClient(**self.connect_options)
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+
+    def write_events(self, snapshot, measurement=None, tags=None):
+        from influxdb_client import Point
+        from influxdb_client.client.exceptions import (
+            InfluxDBError,
+        )
+        from requests import RequestException
+
+        retry_exceptions = (
+            OSError,
+            RequestException,
+            InfluxDBError,
+        )
+
+        if tags:
+            merged_tags = copy(self.tags)
+            merged_tags.update(tags)
+        else:
+            merged_tags = self.tags
+
+        try:
+
+            # if not self.database_created:
+            #    self.client.create_database(self.connect_options["database"])
+            #    self.database_created = True
+            if snapshot:
+                point = Point(measurement or self.default_measurement).time(
+                    datetime.utcnow()
+                )
+                for key, val in merged_tags.items():
+                    point.tag(key, val)
+                for key, val in snapshot.items():
+                    point.field(key, val)
+                while True:
+                    try:
+                        self.write_api.write(
+                            bucket=self.connect_options["bucket"], record=point
+                        )
+                    except retry_exceptions:
+                        logger.exception("Failed to send metrics")
+                        time.sleep(1)
+                        # reconnecting
+                        while True:
+                            try:
+                                self.connect()
+                            except retry_exceptions:
+                                logger.exception(
+                                    "Failed to reconnect to metrics database"
+                                )
+                                time.sleep(1)
+                            else:
+                                break
+                    else:
+                        break
+        except Exception as ex:
+            logging.exception("ERROR IN STAT WRITE EVENTS")
+            raise
