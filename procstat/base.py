06973b33fc45e8c6a6996eaf32d667fa5a5fa877
User <guest@example.com>
2023-07-24 00:45:24 +0300

    Add autofix metric names to prometheus export driver

--- procstat/base.py
+++ procstat/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import re
 import time
 from abc import abstractmethod
 from collections.abc import Mapping
@@ -13,16 +14,20 @@ LOG = logging.getLogger()
 class BaseExportDriver:
     reconnect_interval_sec: float = 1
     retry_exceptions: tuple[type[Exception], ...] = ()
+    re_safe_metric_name = re.compile(r"^[a-zA-Z_:][a-zA-Z0-9_:]*$")
+    re_nonsafe_metric_name_char = re.compile(r"[^a-zA-Z0-9_:]")
 
     def __init__(
         self,
         tags: None | Mapping[str, Any] = None,
         raise_on_error: bool = False,
+        fix_metric_names: bool = True,
     ) -> None:
         self.client = None
         self.tags = deepcopy(tags or {})
         self.database_created = False
         self.raise_on_error = raise_on_error
+        self.fix_metric_names = fix_metric_names
         self.connect()
 
     @abstractmethod
@@ -50,27 +55,35 @@ class BaseExportDriver:
             else:
                 return
 
+    def fix_metric_name(self, name: str) -> str:
+        return self.re_nonsafe_metric_name_char.sub("_", name)
+
     def write_events(
         self,
         snapshot: Mapping[str, int | float | str],
         tags: None | Mapping[str, str | int | float] = None,
-    ) -> None:
-        if not snapshot:
-            return
+    ) -> bool:
         proc_tags = {**self.tags, **tags} if tags else self.tags
+        for metric_name in snapshot:
+            if not self.re_safe_metric_name.match(metric_name):
+                LOG.warning("Non-safe metric name: %s", metric_name)
+        if self.fix_metric_names:
+            snapshot = {self.fix_metric_name(x): y for x, y in snapshot.items()}
         try:
-            while True:
-                try:
-                    self.driver_write_events(
-                        snapshot=snapshot,
-                        tags=proc_tags,
-                    )
-                except self.retry_exceptions:
-                    LOG.exception("Failed to write metrics")
-                    self.repair_connection()
-                else:
-                    break
+            # while True:
+            #    try:
+            self.driver_write_events(
+                snapshot=snapshot,
+                tags=proc_tags,
+            )
+            return True
+            #    except self.retry_exceptions:
+            #        LOG.exception("Failed to write metrics")
+            #        self.repair_connection()
+            #    else:
+            #        break
         except Exception:
-            LOG.exception("ERROR IN STAT WRITE EVENTS")
+            LOG.exception("Failed to export stat data")
             if self.raise_on_error:
                 raise
+            return False
